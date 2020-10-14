022093bbbbdff1667b1cd75cdfd248f99fea5622
motokikasai <kmomoky@hotmail.com>
2020-10-14 22:36:46 +0200

    Use toolkit to create store & reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello Redux Toolkit!</h1>
-    </div>
+    <Router>
+      {/* <Route exact path='/' component={step1} />
+      <Route path='/step2' component={step2} />
+      <Route path='/step3' component={step3} />
+      <Route path='/step4' component={step4} />
+      <Route path='/result' component={result} /> */}
+    </Router>
   );
 }
 
